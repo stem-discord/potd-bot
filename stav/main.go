7e1e16e8001fb9751867ce2c1139fc4b399c9fd5
XpioWolf <95478273+XpioWolf@users.noreply.github.com>
2022-05-18 21:46:29 +0300

    everything prints to stdout now

--- stav/main.go
+++ stav/main.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
 	"time"
 
@@ -32,29 +31,29 @@ func main() {
 	path, _ := os.Getwd()
 	if path[len(path)-8:] != "potd-bot" {
 		fmt.Println("Error, please run in /potd-bot")
-		return
+		os.Exit(1)
 	}
 
 	getEnv()
-	println("Starting up...")
+	fmt.Println("Starting up...")
 
 	//---------set up discord API
 
 	// Creating a new bot session
 	stavBot, err := discord.New("Bot " + token)
 	if err != nil {
-		log.Fatal(err.Error())
-		return
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	// Activating the bot
 	if err = stavBot.Open(); err != nil {
-		log.Fatal(err.Error())
-		return
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 	defer stavBot.Close()
 	stavBot.AddHandler(onNewMessage)
-	println("Bot is running!")
+	fmt.Println("Bot is running!")
 
 	// update global vars
 	activePuzzle, index = refreshActivePuzzle()
@@ -98,29 +97,29 @@ func refreshActivePuzzle() (puzzle, int) {
 	// Read the DB
 	rawDatabase, err := ioutil.ReadFile("./resources/puzzleDB.json")
 	if err != nil {
-		log.Fatal(err.Error())
-		return parsedDB.PuzzleArchive[parsedDB.CurrentPuzzleIndex], parsedDB.CurrentPuzzleIndex
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	// Parse the DB into parsedPuzzleDatabase
 	err = json.Unmarshal(rawDatabase, &parsedDB)
 	if err != nil {
-		log.Fatal(err.Error())
-		return parsedDB.PuzzleArchive[parsedDB.CurrentPuzzleIndex], parsedDB.CurrentPuzzleIndex
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	// Reset all stats
 	err = ioutil.WriteFile("./resources/currentPuzzleStats.json", []byte("{\n    \"answeredUsers\": [],\n    \"correctUsers\": []\n}"), 0644)
 	if err != nil {
-		log.Fatal(err.Error())
-		return parsedDB.PuzzleArchive[parsedDB.CurrentPuzzleIndex], parsedDB.CurrentPuzzleIndex
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
 	return parsedDB.PuzzleArchive[parsedDB.CurrentPuzzleIndex], parsedDB.CurrentPuzzleIndex
 }
 
 func sendNewPOTD(stavBot *discord.Session) {
-	println("Sending POTD...")
+	fmt.Println("Sending POTD...")
 
 	pingMessage := "<@&" + potdRoleID + ">, New puzzle for you to solve!\n"
 
@@ -197,7 +196,7 @@ func sendNewPOTD(stavBot *discord.Session) {
 		Embeds:  embed,
 	})
 	if err != nil {
-		println(err.Error())
+		fmt.Println(err.Error())
 	}
 }
 
@@ -214,13 +213,13 @@ func onNewMessage(session *discord.Session, message *discord.MessageCreate) {
 
 	rawPuzzleStats, err := ioutil.ReadFile("./resources/currentPuzzleStats.json")
 	if err != nil {
-		println(err.Error())
+		fmt.Println(err.Error())
 		return
 	}
 
 	err = json.Unmarshal(rawPuzzleStats, &parsedPuzzleStats)
 	if err != nil {
-		println(err.Error())
+		fmt.Println(err.Error())
 		return
 	}
 
@@ -228,7 +227,7 @@ func onNewMessage(session *discord.Session, message *discord.MessageCreate) {
 		if message.Author.ID == userID {
 			_, err = session.ChannelMessageSend(channel.ID, "Hey! No cheating, you have already answered today, please wait for the next POTD to answer again")
 			if err != nil {
-				println(err.Error())
+				fmt.Println(err.Error())
 			}
 			return
 		}
@@ -243,7 +242,7 @@ func onNewMessage(session *discord.Session, message *discord.MessageCreate) {
 
 	rawPuzzleStats, err = json.MarshalIndent(parsedPuzzleStats, "", "    ")
 	if err != nil {
-		println(err.Error())
+		fmt.Println(err.Error())
 		return
 	}
 
@@ -252,13 +251,13 @@ func onNewMessage(session *discord.Session, message *discord.MessageCreate) {
 		_, err = session.ChannelMessageSend(channel.ID, "Your answer has been recorded! Good luck!")
 
 		if err != nil {
-			println(err.Error())
+			fmt.Println(err.Error())
 		}
 	} else {
 		_, err = session.ChannelMessageSend(channel.ID, "ERROR, We have some kind of database error, please contact @XpioWolf#9420 and they will be on the issue right away.")
 
 		if err != nil {
-			println(err.Error())
+			fmt.Println(err.Error())
 		}
 	}
 }
@@ -267,7 +266,8 @@ func getEnv() {
 	err := godotenv.Load("./stav/.env")
 
 	if err != nil {
-		log.Fatal("Error loading .env file: ", err.Error())
+		fmt.Printf("Error loading .env file: %s", err.Error())
+		os.Exit(1)
 	}
 
 	token = os.Getenv("DISCORD_BOT_TOKEN")
